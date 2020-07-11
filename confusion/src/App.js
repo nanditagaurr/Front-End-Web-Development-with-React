893c2afc124b05e3fe97cc2ed593badcb035b139
nanditagaurr <nandita.17bcs1188@abes.ac.in>
2020-07-12 02:23:07 +0530

    react components

--- confusion/src/App.js
+++ confusion/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
+// eslint-disable-next-line
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
+import { DISHES } from './shared/dishes';
 
-function App() {
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render(){
   return (
     <div className="App">
       <Navbar dark color="primary">
@@ -11,8 +22,10 @@ function App() {
           <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu dishes={this.state.dishes} />
     </div>
   );
+  }
 }
 
 export default App;
